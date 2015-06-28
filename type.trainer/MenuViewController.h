f0b9612d8849196c66a6e45418fcc4d58dfbc2ff
Sergey Mazulev <smazulev@gmail.com>
2015-06-28 14:32:46 +0300

    Update constraints

--- type.trainer/MenuViewController.h
+++ type.trainer/MenuViewController.h
@@ -10,6 +10,20 @@
 
 @interface MenuViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *yourSpeedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *signsPerMinLabel;
+@property (weak, nonatomic) IBOutlet UILabel *wordsPerMinLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mistakesPercentLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *rankLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rankHintLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *starView1;
+@property (weak, nonatomic) IBOutlet UIImageView *starView2;
+@property (weak, nonatomic) IBOutlet UIImageView *starView3;
+@property (weak, nonatomic) IBOutlet UIImageView *starView4;
+@property (weak, nonatomic) IBOutlet UIImageView *starView5;
+
 @property (weak, nonatomic) IBOutlet UIButton *startTypingButton;
 
 @end
